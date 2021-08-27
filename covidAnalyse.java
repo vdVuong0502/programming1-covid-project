b0db7108d3a3e4a897f71d66cef796ce9f8bd06e
Scowboi <bejamin.vnsharing@gmail.com>
2021-08-27 16:20:08 +0700

    add files missing in last commit

---
+++ covidAnalyse.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class covidAnalyse {
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
+
+class Data {
+    public void createData() throws IOException {
+        Scanner fileSc = new Scanner(new File("covid-data.csv"));
+        Scanner dataSc = new Scanner(System.in);
+
+    }
+}
