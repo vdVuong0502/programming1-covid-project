7819c2dee56872dd0eb54f93c816b4ea65ab7a03
Scowboi <bejamin.vnsharing@gmail.com>
2021-08-27 17:06:12 +0700

    add area selection and method exitCheck

--- covidAnalyse.java
+++ covidAnalyse.java
@@ -3,15 +3,29 @@ import java.io.*;
 
 public class covidAnalyse {
     public static void main(String[] args) throws IOException {
+        String selection;
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Welcome to covid data processing and analytics tool.");
+        System.out.println("==============");
+        System.out.println("You can type \"exit\" anytime you want to end the program.");
+        System.out.println("==============");
+        System.out.println("Please specify a geographic area whose data you want to view:");
+        System.out.println("1. By continent");
+        System.out.println("2. By country");
+        selection = userInput.nextLine();
+        exitCheck(selection);
 
     }
 
-}
-
-class Data {
-    public void createData() throws IOException {
-        Scanner fileSc = new Scanner(new File("covid-data.csv"));
-        Scanner dataSc = new Scanner(System.in);
-
+    static void exitCheck(String selection) {// This method check whether user want to quit or not.
+        selection = selection.replace("\n", "").replace("\r", "");
+        selection = selection.toLowerCase();
+        if (selection.equals("exit")) {
+            System.out.println("Thank you for using. The program is exiting.");
+            System.exit(0);
+        }
     }
+
 }
