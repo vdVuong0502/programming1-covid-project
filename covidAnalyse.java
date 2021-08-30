aa35ad3e77f2dc7d9df43be63b4f6eab1b9a0280
Scowboi <bejamin.vnsharing@gmail.com>
2021-08-30 11:18:55 +0700

    create data class and add variable

--- covidAnalyse.java
+++ covidAnalyse.java
@@ -4,18 +4,28 @@ import java.io.*;
 public class covidAnalyse {
     public static void main(String[] args) throws IOException {
         String selection;
-
         Scanner userInput = new Scanner(System.in);
 
         System.out.println("Welcome to covid data processing and analytics tool.");
         System.out.println("==============");
         System.out.println("You can type \"exit\" anytime you want to end the program.");
         System.out.println("==============");
+
+        // Select area
         System.out.println("Please specify a geographic area whose data you want to view:");
         System.out.println("1. By continent");
         System.out.println("2. By country");
         selection = userInput.nextLine();
         exitCheck(selection);
+
+        switch (key) {
+            case value:
+
+                break;
+
+            default:
+                break;
+        }
 
     }
 
@@ -27,5 +37,15 @@ public class covidAnalyse {
             System.exit(0);
         }
     }
+
+}
+
+class DataFilter {
+    String name;
+    ArrayList<String> date = new ArrayList<String>();
+    ArrayList<Integer> newCase = new ArrayList<Integer>();
+    ArrayList<Integer> newDeath = new ArrayList<Integer>();
+    ArrayList<Integer> peopleVacinated = new ArrayList<Integer>();
+    ArrayList<Integer> population = new ArrayList<Integer>();
 
 }
