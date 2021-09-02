b5bd180473a9de53e303ec27d884af99fe0f2901
s3877653 <hotranminhkhoi@gmail.com>
2021-09-02 23:57:49 +0700

    add a chart type display

--- covidAnalyse.java
+++ covidAnalyse.java
@@ -850,13 +850,59 @@ class Summary {
             groupStart = dt.range.get(0);
             groupEnd = dt.range.get((dt.range.size() / sum.groupsNum) - 1);
             for (int i = 0; i < sum.groupsNum; i++) {
-                if (i < dt.range.size() % sum.groupsNum){
-                    
+                if (i < dt.range.size() % sum.groupsNum) {
+
                 }
                 sum.groups.add(groupStart.format(df) + "," + groupEnd.format(df));
                 groupStart = groupEnd.plusDays(1);
                 groupEnd = groupStart.plusDays((dt.range.size() / sum.groupsNum) - 1);
+            }
         }
+
     }
 
 }
+class Display {
+    public ArrayList<String> group = new ArrayList<String>();
+    public ArrayList<Integer> result = new ArrayList<Integer>();
+
+    //choose display method
+    void ChartOption(Scanner sc){
+        int arrLength;
+        String selection;
+        System.out.println("""
+                Please choose a display method:
+                \t1. Tabular display:
+                \t2. Chart display: """);
+        System.out.println(">>>");
+        selection =sc.nextLine();
+        covidAnalyse.exitCheck(selection);
+        //selection invalid
+        while(!(selection.equals(1))&&!(selection.equals(2)))
+    {
+        System.out.println("==================");
+        System.out.println("""
+                Invalid input, please choose again:
+                \t1. Tabular display:
+                \t2. Chart display:""");
+        System.out.print(">>> ");
+        selection = sc.nextLine();
+        covidAnalyse.exitCheck(selection);}
+        //execute user option
+        arrLength = group.size();
+        //tabular type display
+        if(selection.equals(1)){
+            System.out.printf("%-6s%25s","Range","Value");
+            for(int i = 0;i < arrLength; i++){
+                System.out.printf("%-6s%25d\n",group.get(i),result.get(i));
+            }
+        }
+        //chart type display
+        else{
+
+        }
+
+
+    }
+}
+
